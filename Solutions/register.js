b210ccacd2e05cead240329e4e22a696b1a2ac30
Paul Herold Jr <pjhd12@mit.edu>
2016-08-20 16:13:37 -0400

    Web app and solutions

--- Solutions/register.js
+++ Solutions/register.js
@@ -1,8 +1,14 @@
-const key = 'dc627becd123734adc4f2033c38ec414'
-const url1 = 'http://challenge.code2040.org/api/dating';
-const url2 = url1 + '/validate';
+const request = require('request');
 
-fetch(url1, {
-	method: 'POST', 
-	body: { token: key }
-}).then(x => console.log(x));
+const key = 'dc627becd123734adc4f2033c38ec414';
+const url = 'http://challenge.code2040.org/api/register';
+const github = 'https://github.com/pjherold/Code2040';
+request.post(
+	url,
+	{ form: { token: key, github: github } },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+        }
+    }
+);
